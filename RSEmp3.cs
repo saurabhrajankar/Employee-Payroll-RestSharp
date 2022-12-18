43fc64fa56d8a19faa91d36b15836f65acac6a5f
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-12-18 16:05:26 +0530

    Add Multiple Employees Using RestSharp

---
+++ RSEmp3.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace RSEmp3
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+    public class Operation
+    {
+        RestClient restClient = new RestClient("http://localhost:4000");
+        public void AddMultipleEmployee()
+        {
+            List<Employee> employee = new List<Employee>();
+            employee.Add(new Employee { Name = "FFF", Salary = "7000" });
+            employee.Add(new Employee { Name = "GGG", Salary = "8000" });
+            employee.Add(new Employee { Name = "HHH", Salary = "9000" });
+            employee.Add(new Employee { Name = "III", Salary = "10000" });
+
+            foreach(Employee emp in employee)
+            {
+                RestRequest rq = new RestRequest("/employee", Method.POST);
+                JObject jb = new JObject();
+                jb.Add("name", emp.Name);
+                jb.Add("salary", emp.Salary);
+                rq.AddParameter("application/json", jb, ParameterType.RequestBody);
+                IRestResponse rr=restClient.Post(rq);
+            }
+        }
+    }
+    public class RSEmp3
+    {
+        static void Main(string[] args)
+        {
+            Operation obj=new Operation();
+            obj.AddMultipleEmployee();
+        }
+    }
+}
