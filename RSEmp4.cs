804e6c8b9382f027d06699141eca58bb3a6a1d21
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-12-18 16:06:22 +0530

    Update Salary Of Employee Using RestSharp

---
+++ RSEmp4.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+
+namespace RSEmp4
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+    public class Operation
+    {
+        RestClient restClient = new RestClient("http://localhost:4000");
+
+        public void UpdateEmployeeSalary()
+        {
+            RestRequest rq=new RestRequest("/employee/109",Method.PUT);
+            JObject jb = new JObject();
+            jb.Add("name", "III");
+            jb.Add("salary", "11000");
+            rq.AddOrUpdateParameter("application/json", jb, ParameterType.RequestBody);
+
+            IRestResponse rr=restClient.Put(rq);
+        }
+    }
+    public class RSEmp4
+    {
+        static void Main(string[] args)
+        {
+            Operation obj= new Operation();
+            obj.UpdateEmployeeSalary();
+        }
+    }
+}
