06094d4bf0498670a2efd2919645c7e5c14f5c79
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-12-18 16:02:47 +0530

    Add Employee Using RestSharp

---
+++ RSEmp2.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Xml.Linq;
+
+namespace RSEmp2
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+    public class Operation
+    {
+        RestClient restClient = new RestClient("http://localhost:4000");
+
+        public void AddEmployee()
+        {
+            RestRequest restRequest = new RestRequest("/employee", Method.POST);
+            JObject jb = new JObject();
+            jb.Add("name", "EEE");
+            jb.Add("salary", 6000);
+            restRequest.AddParameter("application/json", jb, ParameterType.RequestBody);
+
+            IRestResponse restResponse = restClient.Post(restRequest);
+        }
+    }
+    public class RSEmp2
+    {
+        static void Main(string[] args)
+        {
+            Operation obj= new Operation();
+            obj.AddEmployee();
+        }
+    }
+}
